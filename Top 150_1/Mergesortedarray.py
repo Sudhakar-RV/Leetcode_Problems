980e0d33fb1f35d878c6f93fd7bddbb8ffe295ef
sivashankarikrishnaraj27 <sivashankarikrishnaraj27@gmail.com>
2026-09-18 16:25:36 +0530

    Normal-1, Normal-2, top150-1, top150-2

---
+++ Top 150_1/Mergesortedarray.py
@@ -0,0 +1,9 @@
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        nums1[:]=nums1[:m]
+        nums1[:]=nums1+nums2
+        nums1.sort()
