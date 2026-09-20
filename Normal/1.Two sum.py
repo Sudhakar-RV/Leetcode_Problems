9e9e585d7127271296ee4e4015de9d3b59125792
Sudhakar-RV <sudhakar.rv.tech@gmail.com>
2026-09-20 14:33:42 +0530

    Optimised

--- Normal/1.Two sum.py
+++ Normal/1.Two sum.py
@@ -1,10 +1,18 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        left = 0
-        right = 0
-        for left in range(len(nums)-1):
-            for right in range(left+1,len(nums)):
-                if nums[left]+nums[right] == target:
-                    return [left,right]
+        seen = {}
+        for i in range(len(nums)):
+            need = target - nums[i]
+            if need in seen:
+                return [seen[need],i]
+            else:
+                seen[nums[i]] = i
+        
 
+
+
+        
+
+
+        
         
